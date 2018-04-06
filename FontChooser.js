183d373f8daef180ac92769989cf84a8a1ac48ff
q_bit <q_bit@icloud.com>
2018-04-06 15:55:10 -0400

    Removed hard coded value for default font size. Added font color when min and max reached. Added double click event

--- FontChooser.js
+++ FontChooser.js
@@ -2,7 +2,9 @@ class FontChooser extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {hidden: true, bold: false, fontSize: 16};
+		this.state = {hidden: true, 
+                  bold: false, 
+                  fontSize: this.props.size};
 	}
 
 	showControls() {
@@ -24,22 +26,30 @@ class FontChooser extends React.Component {
 			Number(this.state.fontSize) + 1 : this.props.max;
 		this.setState({fontSize: size});
 	}
+	
+	reset() {
+		this.setState( {fontSize: this.props.size} );
+	}
 
 	render() {
 		var weight = this.state.bold ? 'bold' : 'normal';
+		var color = (this.state.fontSize == this.props.min || 
+								 this.state.fontSize == this.props.max) ? 'red':'black';
 		return ( 
 			<div>
 			<input type = "checkbox" id = "boldCheckbox" hidden = {this.state.hidden} 
 				onClick = {this.toggleBold.bind(this)}/>
 			<button id = "decreaseButton" hidden = {this.state.hidden} 
 				onClick = {this.decreaseFont.bind(this)}> - </button>
-			<span id = "fontSizeSpan" hidden = {this.state.hidden} > {this.state.fontSize}</span> 
+			<span id = "fontSizeSpan" hidden = {this.state.hidden} 
+				onDoubleClick = {this.reset.bind(this)}> {this.state.fontSize}</span> 
 			<button id = "increaseButton" hidden = {this.state.hidden}
 				onClick = {this.increaseFont.bind(this)} > + < /button> 
 			<span id = "textSpan" onClick = {this.showControls.bind(this)}
 				style = { {
 					fontWeight: weight,
-					fontSize: this.state.fontSize
+					fontSize: this.state.fontSize,
+					color: color
 				} } > {this.props.text} < /span> 
 			< /div >
 		);
